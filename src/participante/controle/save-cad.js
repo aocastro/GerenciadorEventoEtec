9d1062966337998f64924eefd12a7c3b5264f95d
AyaneYamassaki <karenayane4703@hotmail.com>
2021-11-24 21:15:07 -0300

    Mais algumas alreções

---
+++ src/participante/controle/save-cad.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+    $('.btn-save').click(function(e) {
+        e.preventDefault()
+
+        let dados = $('#form-participante').serialize()
+
+        dados += `&operacao=${$('.btn-save').attr('data-operation')}`
+
+        $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            assync: true,
+            data: dados,
+            url: 'src/participante/modelo/save-participante.php',
+            success: function(dados) {
+                Swal.fire({
+                    title: 'Elevent',
+                    text: dados.mensagem,
+                    icon: dados.tipo,
+                    confirmButtonText: 'OK'
+                })
+            }
+        })
+    })
+
+})
